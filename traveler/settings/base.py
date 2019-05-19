d764337747ab28e51229abc74f4f3889fe09e0f0
Mimoun Bijjou <mimounbijjou@Mimouns-Air.fritz.box>
2019-05-19 09:42:59 +0200

    added template and media

--- traveler/settings/base.py
+++ traveler/settings/base.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'traveler.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,3 +130,6 @@ REST_FRAMEWORK = {
         #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ]
 }
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
