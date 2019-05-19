06499919f6ad1b8b2992992bcd2febd7aced7b28
Mimoun Bijjou <mimounbijjou@Mimouns-Air.fritz.box>
2019-05-19 09:49:49 +0200

    change settings

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'traveler.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'traveler.settings.staging')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
