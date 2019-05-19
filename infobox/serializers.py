4b2056b73fcfd11ed4518ff7b6715fa704c616c2
Mimoun Bijjou <mimounbijjou@Mimouns-Air.fritz.box>
2019-05-19 05:29:06 +0200

    deploy heroku

--- infobox/serializers.py
+++ infobox/serializers.py
@@ -12,3 +12,4 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'title', 'parent', "child_categories", "infoboxes",)
+        #  need to deploy
