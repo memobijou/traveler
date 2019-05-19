6f5a79f1a90fa2567533ebb01acda4a380257d34
Mimoun Bijjou <mimounbijjou@Mimouns-Air.fritz.box>
2019-05-19 05:44:41 +0200

    static folder

--- traveler/urls.py
+++ traveler/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -22,3 +24,8 @@ urlpatterns = [
     path(r'api/v1/api-auth/', include('rest_framework.urls')),
     path(r'api/v1/', include('infobox.api_urls')),
 ]
+
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
